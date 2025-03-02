9e62b60e9b512774e877e001ee5b92a4b2e89987
Shengtian Liao <shengtian.l@fcl.software>
2025-03-02 21:38:34 +1100

    refactor: update AuthService to use UserService for user management

--- src/game/auth.service.ts
+++ src/game/auth.service.ts
@@ -3,12 +3,14 @@ import { ClientModel } from "src/common/models/client.model";
 import { UserModel } from "src/common/models/user.model";
 import { SignInRequest } from "src/common/protocols/apis.models";
 import { RoomService } from "./room.service";
+import { UserService } from "./user.service";
 
 @Injectable()
 export class AuthService {
-  public users: UserModel[] = [];
-
-  constructor(private roomService: RoomService) {}
+  constructor(
+    private userService: UserService,
+    private roomService: RoomService,
+  ) {}
 
   signIn(data: SignInRequest["data"], client: ClientModel): UserModel {
     // check user is signed in or not
@@ -26,17 +28,16 @@ export class AuthService {
       throw new Error("Email format is invalid");
     }
 
-    let user = this.users.find((user) => user.email === data.email);
+    let user = this.userService.find(data.email);
 
     if (!user) {
-      user = new UserModel({
+      user = this.userService.create({
         name: data.email,
         firstName: data.email.split("@")[0],
         lastName: data.email.split("@")[1],
         email: data.email,
       });
       user.password = data.password;
-      this.users.push(user);
     }
 
     if (user.password !== data.password) {
